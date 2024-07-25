4ef2dd4a826c451ca9330ce1bbd699219bb96ac7
uchejoseph57 <114272758+uchejoseph57@users.noreply.github.com>
2024-07-24 21:39:57 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install nginx -y 
 sudo systemctl enable nginx
 sudo systemctl start nginx
-git clone https://git.toptal.com/screening-ops/Anthony-ahamefule.git
+git clone https://git.toptal.com/screening-ops/Joseph-oluigboka
+.git
 cd /Anthony-ahamefule
 pip install requirements.txt
 echo "*/10 * * * * $SHELL remediation.sh" | crontab -
